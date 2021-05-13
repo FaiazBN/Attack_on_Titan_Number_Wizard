3c21249a8805fe5c72139bb2b43761e31ecb2c4d
FaiazBN <faiaz.nesar@gmail.com>
2021-05-13 12:39:26 +0600

    Initial Push

---
+++ Assets/Scenes/Scripts/ConversationPart.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ConversationPart : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI textComponent;
+    [SerializeField] State startingState;
+
+    State state;
+
+    void Start()
+    {
+        state = startingState;
+        textComponent.text = state.GetStoryState();
+
+
+    }
+
+    
+    void Update()
+    {
+        ManangeStates();
+
+    }
+    private void ManangeStates()
+    {
+        var nextStates = state.GetNextState();
+
+        for (int i = 0; i < nextStates.Length; i++) {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                state = nextStates[0];
+            }
+        }
+        
+        textComponent.text = state.GetStoryState();
+    }
+}
---
+++ Assets/Scenes/Scripts/SceneLoader.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+   public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        SceneManager.LoadScene(currentSceneIndex + 1);
+
+    }
+    public void LoadPrevScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        SceneManager.LoadScene(currentSceneIndex - 1);
+
+    }
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void LoadTryAgain()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void LoadInstructuons()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+}
